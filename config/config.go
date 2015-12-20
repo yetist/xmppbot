4d40e9edcf8d1cb56356a5eb36cce586e35f8762
yetist <xiaotian.wu@i-soft.com.cn>
2015-12-20 10:58:09 +0800

    update

--- config/config.go
+++ config/config.go
@@ -10,34 +10,6 @@ import (
 	"strings"
 )
 
-type Config struct {
-	AppName    string
-	AppVersion string
-	AppConfig  string
-	Account    struct {
-		Username string
-		Password string
-		Resource string
-		Server   string
-		Port     int
-		NoTLS    bool
-		SelfSign bool `toml:"self_signed"`
-		Session  bool
-	}
-	Setup struct {
-		Admin         []string
-		Debug         bool
-		AutoSubscribe bool   `toml:"auto_subscribe"`
-		CmdPrefix     string `toml:"cmd_prefix"`
-		Status        string
-		StatusMessage string `toml:"status_message"`
-		WebHost       string `toml:"web_host"`
-		WebPort       int    `toml:"web_port"`
-		Rooms         []map[string]interface{}
-	}
-	Plugin map[string]map[string]interface{}
-}
-
 func selfConfigDir() string {
 	if dir, err := utils.GetExecDir(); err != nil || strings.HasSuffix(dir, "_obj/exe") {
 		wd, _ := os.Getwd()
